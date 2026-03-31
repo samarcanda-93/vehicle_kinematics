c97ebd33c93eda896008a981f629135443c0e5fe
Giacomo Accorto <gaccorto@proton.me>
2026-03-31 16:00:50 +0200

    chore: last commit for now

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ from vehicle_kinematics import parse, run_pipeline
 
 if __name__ == "__main__":
     data_path = Path("data/input_coordinates.txt")
-    data = parse(data_path)
-    run_pipeline(data, h=1500.0)
+    run_pipeline(parse(data_path), h=1500.0)
